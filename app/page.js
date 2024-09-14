4b1f4abc9d91518be9baa9e56f1b646e89e3cbbf
SUNIL DUTT <sdutt8797@gmail.com>
2024-09-14 16:36:27 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Link from 'next/link'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Footer from './components/Footer'
+import React from "react";
+import Link from "next/link";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 const page = () => {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <Navbar />
+      <Home />
+      hello how are you
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
